5ae263f8c93b5319eb72030f39338de8e957ddc1
wolf1729 <aryanm1729@gmail.com>
2024-04-19 11:00:35 +0530

    new commits

--- frontend/src/Components/buyerDashboard.jsx
+++ frontend/src/Components/buyerDashboard.jsx
@@ -51,7 +51,7 @@ function BuyerDashboard() {
 
     gettingBuyerDetails()
     getAllProducts()
-  }, [user.buyerId, productArray])
+  })
 
   const searchingProductWithNameFunction = async() => {
     try{
